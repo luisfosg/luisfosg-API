4fdfa0e0d44a904aacfa8c45204f8f6ff85bdfb2
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-01-13 18:46:14 -0500

    creando request.http y enviando primeras peticiones

--- src/controllers/proyects-controller.js
+++ src/controllers/proyects-controller.js
@@ -1,3 +1,5 @@
+import Proyect from '../models/proyects';
+
 export const obtenerProyectos = (req, res) => {
     res.status(200).json({"status": "OK"})
 }
@@ -7,7 +9,8 @@ export const obtenerProyecto = (req, res) => {
 }
 
 export const agregarProyecto = (req, res) => {
-    res.status(200).json({"status": "OK"})
+    console.log(req.body);
+    res.status(200).json({"status": "Proyect Created"})
 }
 
 export const editarProyecto = (req, res) => {
