2db562746fd2f25f215ebf5b88069b0737003667
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-01-13 21:54:55 -0500

    CRUD de peticiones (proyectos) listo.

--- src/controllers/proyects-controller.js
+++ src/controllers/proyects-controller.js
@@ -1,29 +1,34 @@
-import Proyect from '../models/proyects';
+import Proyect from "../models/proyects";
 
-export const obtenerProyectos = async (req, res) => {
-    const proyects =  await Proyect.find();
+export const obtenerProyectos = async (_req, res) => {
+    const proyects = await Proyect.find();
 
     res.status(200).json(proyects);
-}
+};
 
-export const obtenerProyecto = (req, res) => {
-    res.status(200).json({"status": "OK"})
-}
+export const obtenerProyecto = async (req, res) => {
+    const proyect = await Proyect.findById(req.params.id);
+
+    res.status(200).json(proyect);
+};
 
 export const agregarProyecto = async (req, res) => {
-    const {name, imgUrl, description, github, url} = req.body;
+    const { name, imgUrl, description, github, url } = req.body;
 
-    const newProyect = new Proyect({name, imgUrl, description, github, url});
+    const newProyect = new Proyect({ name, imgUrl, description, github, url });
 
     const proyectSave = await newProyect.save();
 
     res.status(201).json(proyectSave);
-}
+};
 
-export const editarProyecto = (req, res) => {
-    res.status(200).json({"status": "OK"})
-}
+export const editarProyecto = async (req, res) => {
+    const { name, imgUrl, description, github, url } = req.body;
+    const updateProyect = await Proyect.findOneAndUpdate(req.params.id, { name, imgUrl, description, github, url }, { new: true });
+    res.status(200).json(updateProyect);
+};
 
-export const eliminarProyecto = (req, res) => {
-    res.status(200).json({"status": "OK"})
-}
+export const eliminarProyecto = async (req, res) => {
+    const deleteProyect = await Proyect.findByIdAndDelete(req.params.id);
+    res.status(200).json(deleteProyect);
+};
--- src/database.js
+++ src/database.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 mongoose.connect("mongodb://localhost/luisfosg-api", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    useFindAndModify: false
 })
-    .then(db => console.log("\n Db is Connected \n"))
+    .then(_db => console.log("\n Db is Connected \n"))
     .catch(err => console.log(err))
