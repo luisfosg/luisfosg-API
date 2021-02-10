cea8151cfb87e3e8584d4b4d110fd0f6a04481fb
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-02-10 16:56:01 -0500

    model de los emails

--- src/models/emails.js
+++ src/models/emails.js
@@ -1,6 +1,9 @@
 import { Schema, model } from 'mongoose';
 
 const emailSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    menssage: { type: String, required: true },
 }, {
     timestamps: true,
     versionKey: false
