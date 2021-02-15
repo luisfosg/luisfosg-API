5165c6a36c94d205406872271911f68477b1568c
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-02-15 16:01:46 -0500

    services: nodemailer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from 'path';
 
 import pkg from '../package.json';
 
@@ -11,6 +12,7 @@ app.set("port", process.env.PORT || 2001);
 
 app.use(morgan("dev"));
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
---
+++ src/services/mailer.js
@@ -0,0 +1,31 @@
+import Mailer from 'nodemailer';
+
+const conexionEmail = Mailer.createTransport({
+    service: "gmail",
+    auth:{
+        user: "@gmail.com",
+        pass: ""
+    }
+});
+
+export const sendEmail = async (from, msg, name, error) => {
+    let mailOptions = {
+        from: "@gmail.com",
+        to: "@gmail.com",
+        subject: "Asunto",
+        text: msg,
+        html: `
+            <h1>${ name }</h1>
+            <h3>${ from }</h3>
+        `,
+    };
+
+    conexionEmail.sendMail(mailOptions, function (err, info) {
+        if (err) {
+            return error;
+        }
+        else {
+            return info.response;
+        }
+    });
+}
