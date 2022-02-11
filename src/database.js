e6a2bb10ea40d8cb3f4be5852a71f0b3af467e9f
LuisFOsG <luisfosgdev@gmail.com>
2022-02-10 19:38:41 -0500

    fix(Root): update mongo connection

--- src/database.js
+++ src/database.js
@@ -2,18 +2,13 @@ import mongoose from 'mongoose';
 
 const { MONGOCONNECTION, MONGOCONNECTION_TEST, NODE_ENV } = process.env;
 
-let connection;
-if ( NODE_ENV === 'production' ) {
-	connection = MONGOCONNECTION;
-} else {
-	connection = MONGOCONNECTION_TEST;
-}
+const connection = NODE_ENV === 'production' ? MONGOCONNECTION : MONGOCONNECTION_TEST;
 
-mongoose.connect( connection, {
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-	useFindAndModify: false,
-	useCreateIndex: true,
-} )
-	.then( ( _db ) => console.log( `\n Db is Connected in  ${ NODE_ENV }` ) )
-	.catch( ( _err ) => console.log( `\n Db has an error in ${ NODE_ENV }` ) );
+mongoose.connect( connection )
+	.then( ( _db ) => console.log( `\n Db is Connected in ${ NODE_ENV }` ) )
+	.catch( ( _err ) => console.log( `\n Db has an error: ${ _err }` ) );
+
+process.on( 'uncaughtException', () => {
+	mongoose.connection.close();
+	process.exit( 1 );
+} );
